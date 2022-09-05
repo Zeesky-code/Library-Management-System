cb75999d07ef54ce926baa819202e1e2d5ddc9e4
Zainab Lawal <zainablaw1012@gmail.com>
2022-09-06 01:51:34 +0300

    Switched to Express

--- index.js
+++ index.js
@@ -1,56 +1,44 @@
-const http = require("http");
-const path = require("path");
-const { readFileSync } = require('fs');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const {authenticateUser} = require("./authenticate")
-// get all files
-const homePage = readFileSync(path.join(__dirname, "public", "index.html"))
-const homeStyles = readFileSync(path.join(__dirname, "public","css" ,"style.css"))
-const loginPage = readFileSync(path.join(__dirname, "public", "login.html"))
-const loginStyles = readFileSync(path.join(__dirname, "public","css" ,"login.css"))
+const booksRoute = require('./routes/books');
+const authorRoute = require('./routes/authors');
+
+const port = 8000;
+const app = express();
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+//setting express to use the database
+
+app.use('/books', booksRoute);
+app.use('/authors', authorRoute)
+
+
 
 const hostname = 'localhost';
-const port = 8000;
-const server = http.createServer((req, res) => {
-    switch (req.url){
-        case '/':
-            res.writeHead(200, { 'content-type': 'text/html' })
-            res.write(homePage)
-            res.end()
-            break;
-        case '/css/style.css':
-            res.writeHead(200, { 'content-type': 'text/css' })
-            res.write(homeStyles)
-            res.end()
-            break;
-        case '/css/login.css':
-            res.writeHead(200, { 'content-type': 'text/css' })
-            res.write(loginStyles)
-            res.end()
-            break;
-        case '/login':
-            res.writeHead(200, { 'content-type': 'text/html' })
-            res.write(loginPage)
-            res.end()
-            break;
-        case '/auth':
-            authenticateUser(req, res)
-                .then(()=> {
-                    res.write('Welcome')
-                }).catch((err)=> {
-                    res.writeHead(400)
-                    res.end(JSON.stringify({
-                        message: err
-                    }))
-                })
-            break;
-        default:
-            res.writeHead(404, { 'content-type': 'text/html' })
-            res.write('<h1>page not found</h1>')
-            res.end()
-    }
+
+//routes with express
+app.get('/', (req, res)=>{
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write(homePage)
+    res.end()
+})
+app.get('/auth', (req, res)=>{
+    authenticateUser(req, res)
+        .then(()=> {
+            res.write('Welcome')
+        }).catch((err)=> {
+            res.writeHead(400)
+            res.end(JSON.stringify({
+                message: err
+            }))
+        })
 })
 
-server.listen(port, hostname, () => {
+
+
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 })
